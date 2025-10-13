fe3ddbc7dc66f1e056e01594e435125e6c335fb4
Mikle <m.michele.2003@gmail.com>
2025-10-13 11:56:53 +0200

    bevy: fix bot bumpers

--- bevy-experiment/src/main.rs
+++ bevy-experiment/src/main.rs
@@ -399,8 +399,8 @@ fn handle_motors_input(
         0.0
     };
 
-    const FORWARD_TORQUE: f32 = 10.1;
-    const SIDE_TORQUE: f32 = 10.1;
+    const FORWARD_TORQUE: f32 = 0.01;
+    const SIDE_TORQUE: f32 = 0.01;
 
     torque.left_torque = forward * FORWARD_TORQUE + side * SIDE_TORQUE;
     torque.right_torque = forward * FORWARD_TORQUE - side * SIDE_TORQUE;
@@ -548,7 +548,6 @@ const BOT_BODY_LENGHT_MIN: f32 = 0.04;
 const BOT_BODY_LENGHT_PERCENT_OF_TOTAL: f32 = 0.6;
 const BOT_BODY_WIDTH: f32 = 0.09;
 const BOT_BODY_HEIGHT: f32 = 0.02;
-const BOT_OFFSET_Y_FRONT_LENGHT_PERCENT: f32 = 0.1;
 
 const BOT_BUMPER_DIAMETER: f32 = BOT_BODY_HEIGHT / 2.0;
 const BOT_BUMPER_WIDTH: f32 = BOT_BODY_WIDTH / 2.0;
@@ -573,7 +572,11 @@ fn setup_bot(mut commands: Commands) {
     // Height of line sensors from the ground (in mm, from 1 to wheels radius)
     let front_sensors_height: f32 = 2.0 / 1000.0;
 
-    let body_offset_z: f32 = clearing_back;
+    let body_world = Vec3::new(
+        0.0,
+        (length_front - length_back) / 2.0,
+        clearing_back + (BOT_BODY_HEIGHT * 0.5),
+    );
 
     // Static body with motors
     let body = commands
@@ -591,11 +594,7 @@ fn setup_bot(mut commands: Commands) {
                 combine_rule: CoefficientCombineRule::Min,
             },
             ColliderMassProperties::Density(1.0),
-            Transform::from_xyz(
-                0.0,
-                BOT_OFFSET_Y_FRONT_LENGHT_PERCENT * length_front,
-                (body_offset_z + BOT_BODY_HEIGHT) * 0.5,
-            ),
+            Transform::from_xyz(body_world.x, body_world.y, body_world.z),
             GlobalTransform::default(),
             Motors {
                 left_axle: Vec3::X,
@@ -606,30 +605,31 @@ fn setup_bot(mut commands: Commands) {
         ))
         .id();
 
-    commands.spawn((
-        Collider::compound(vec![(
-            Vec3::ZERO,
-            Quat::from_rotation_z(FRAC_PI_2),
-            Collider::cylinder(BOT_BUMPER_WIDTH / 2.0, BOT_BUMPER_DIAMETER / 2.0),
-        )]),
-        RigidBody::Dynamic,
-        Friction {
-            coefficient: 0.1,
-            combine_rule: CoefficientCombineRule::Min,
-        },
-        Transform::from_xyz(0.0, length_front, BOT_BUMPER_DIAMETER / 2.0),
-        GlobalTransform::default(),
-        ImpulseJoint::new(
-            body,
-            FixedJointBuilder::new()
-                .local_anchor1(Vec3::new(0.0, length_front, BOT_BUMPER_DIAMETER / 2.0))
-                .local_anchor2(Vec3::new(
-                    0.0,
-                    BOT_OFFSET_Y_FRONT_LENGHT_PERCENT * length_front,
-                    body_offset_z + BOT_BODY_HEIGHT * 0.5,
-                )),
-        ),
-    ));
+    // Cylinder bumpers
+    let front_bumper_world = Vec3::new(0.0, length_front, BOT_BUMPER_DIAMETER / 2.0);
+    let back_bumper_world = Vec3::new(0.0, -length_back, BOT_BUMPER_DIAMETER / 2.0 + clearing_back);
+
+    for bumper_world in [front_bumper_world, back_bumper_world] {
+        commands.spawn((
+            Collider::compound(vec![(
+                Vec3::ZERO,
+                Quat::from_rotation_z(FRAC_PI_2),
+                Collider::cylinder(BOT_BUMPER_WIDTH / 2.0, BOT_BUMPER_DIAMETER / 2.0),
+            )]),
+            RigidBody::Dynamic,
+            Friction {
+                coefficient: 0.1,
+                combine_rule: CoefficientCombineRule::Min,
+            },
+            Transform::from_xyz(bumper_world.x, bumper_world.y, bumper_world.z),
+            ImpulseJoint::new(
+                body,
+                FixedJointBuilder::new()
+                    .local_anchor1(bumper_world - body_world) // parent's local anchor
+                    .local_anchor2(Vec3::ZERO),
+            ),
+        ));
+    }
 
     // let wheel_joints_x = 1.5;
 
@@ -680,18 +680,16 @@ fn setup_bot(mut commands: Commands) {
     //     .id();
 }
 
-fn setup_track(commands: Commands, track: Res<Track>) {
-    track.spawn_bundles(commands);
-
+fn setup_track(mut commands: Commands, track: Res<Track>) {
     // Static floor
-    // let _floor = commands
-    //     .spawn((
-    //         Collider::cuboid(0.5, 0.5, 0.5),
-    //         RigidBody::Fixed,
-    //         Friction::new(0.5),
-    //         Transform::from_xyz(0.0, 0.0, -4.0).with_scale(Vec3::new(50.0, 50.0, 0.1)),
-    //     ))
-    //     .id();
+    commands.spawn((
+        Collider::cuboid(10.0, 10.0, 0.1),
+        RigidBody::Fixed,
+        Friction::new(0.5),
+        Transform::from_xyz(0.0, 0.0, -0.1),
+    ));
+
+    // track.spawn_bundles(commands);
 }
 
 fn setup_ui(mut commands: Commands) {
@@ -715,12 +713,12 @@ fn setup_ui(mut commands: Commands) {
             },
             ..Default::default()
         },
-        Transform::from_translation(Vec3::Z * 1.0).looking_at(
-            Vec3::Y,
+        Transform::from_translation(Vec3::X * 0.5).looking_at(
+            Vec3::X,
             Vec3 {
                 x: 0.0,
-                y: 0.5,
-                z: 1.0,
+                y: 0.0,
+                z: 0.1,
             },
         ),
     ));
